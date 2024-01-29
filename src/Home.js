0a33f2f3cf2cc02b0ea4424ec5f857091dab7d63
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2024-01-29 17:41:02 +0530

    done

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import HomeImage from "../src/Images/Homes.png"; 
 
 function Home() {
   return (
@@ -8,10 +9,10 @@ function Home() {
         <div className='Hero'>
           <h1>We @@Design, Develop and Manufacture @@@ Integrated Charging Solutions</h1>
           <p>A vision to integrate USB charging to every bedside and workspace.</p>
-          <button style={{ backgroundColor: 'green', height:"70px", width:"250px", borderRadius:"30px" }}>Get in Touch</button>
+          <button style={{ backgroundColor: 'green', height: "70px", width: "250px", borderRadius: "30px" }}>Get in Touch</button>
         </div>
-        <div className="ImageContainer" style={{ backgroundColor: 'light-green' }}>
-          <img src="https://imgur.com/NnKX4j8.png" />
+        <div className="ImageContainer" style={{ backgroundColor: 'lightgreen' }}>
+          <img src={HomeImage} alt="Home" />
         </div>
       </div>
     </>
